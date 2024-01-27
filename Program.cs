202c8da590df035e8668d56e656da9f26e384b3c
DavidGFonseca <david.gfonseca.92@gmail.com>
2024-01-27 16:03:39 -0400

    Primeiro commit da versão inicial do projeto

---
+++ Program.cs
@@ -0,0 +1,112 @@
+﻿string logoSoundDisc = @"
+░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░██████╗░██╗░██████╗░█████╗░
+██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗██╔══██╗██║██╔════╝██╔══██╗
+╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║██║░░██║██║╚█████╗░██║░░╚═╝
+░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║██║░░██║██║░╚═══██╗██║░░██╗
+██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝██████╔╝██║██████╔╝╚█████╔╝
+╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░╚═════╝░╚═╝╚═════╝░░╚════╝░";
+string welcome = "\nSeja bem vindo!!!";
+
+// List<string> listBandas = new List<string>();
+Dictionary<string,List<float>> listBandas = new Dictionary<string, List<float>>();
+
+ExibirMensagens(logoSoundDisc);
+ExibirMensagens(welcome);
+
+ListOptionMenu();
+
+void ExibirMensagens(string mensagem)
+{
+    Console.WriteLine(mensagem);
+}
+
+void ListOptionMenu()
+{
+
+    ExibirMensagens("\nOpção 1 - Registrar uma banda");
+    ExibirMensagens("\nOpção 2 - Mostrar todas as bandas");
+    ExibirMensagens("\nOpção 3 - Avaliar uma bandas");
+    ExibirMensagens("\nOpção 4 - Media de uma banda");
+    ExibirMensagens("\nOpção 9 - Sair\n");  
+    string optionStr = Console.ReadLine()!;
+    int optionInt = int.Parse(optionStr);
+    VerifyOption(optionInt);
+}
+
+void VerifyOption(int option)
+{
+    switch (option)
+    {
+        case 1: RegistreBanda();
+            break;
+        case 2: ListaBandas();
+            break;
+        case 3: AvaliarBanda();
+            break;
+        case 4: MediaBanda();
+            break;
+        default: ExibirMensagens("Opção invalida");
+            break;
+    }
+}
+
+void RegistreBanda()
+{
+    ExibirMensagens("Informe nome da Banda: \n");
+    string newBanda = Console.ReadLine()!;
+    listBandas.Add(newBanda, new List<float>());
+    Console.Clear();
+    ListOptionMenu();
+}
+
+void ListaBandas()
+{
+    ExibirMensagens("Lista de Bandas\n");
+    foreach (string banda in listBandas.Keys)
+    {
+        Console.WriteLine($"Banda: {banda}");
+    }
+    Thread.Sleep(2000);
+    Console.Clear();
+    ListOptionMenu();
+}
+
+void AvaliarBanda()
+{
+    Console.WriteLine("Qual banda deseja avaliar: \n");
+    string bandaChoised = Console.ReadLine()!;
+    if (listBandas.ContainsKey(bandaChoised))
+    {
+        Console.WriteLine($"Informe da nota desejada para a banda {bandaChoised}: ");
+        string notaString = Console.ReadLine()!;
+        float notaBanda = float.Parse(notaString);
+        listBandas[bandaChoised].Add(notaBanda);
+        Console.WriteLine(notaBanda);
+        Console.Clear();
+
+        ListOptionMenu();
+    } else
+    {
+        ExibirMensagens($"Banda {bandaChoised} não foi encontrada!");
+
+    }
+}
+
+void MediaBanda()
+{
+    Console.WriteLine("Qual banda deseja verificar a média das avaliações: \n");
+    string bandaChoised = Console.ReadLine()!;
+    if (listBandas.ContainsKey(bandaChoised))
+    {
+        float notaTotal = 0;
+        foreach (float notaAtual in listBandas[bandaChoised])
+        {
+            notaTotal += notaAtual;
+
+        }
+        float notaMedia = notaTotal / listBandas[bandaChoised].Count;
+        Console.WriteLine(notaMedia);
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,14 @@
+<h1>Projeto de API em C#</h1>
+
+<h3>Etapas do projeto</h3>
+
+- [] Entendendo a linguagem e plataforma .net <br>
+- [] Refatorando a implementação adotando os metodos:<br>
+  - [] SOLID<br>
+  - [] Desing Patter - Strategy<br>
+  - [] MVC<br>
+- [] Criar rotas<br>
+- [] Criar um Front-End em React<br>
+- [] Conectar Front com Back-end<br>
+
+<span>Este projeto tem a finalidade de praticar meus conhecimentos sobre a linguagem C# e a plataforme .Net</span>
